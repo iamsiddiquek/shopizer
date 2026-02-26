bf9b9b5bd6e43c78f5c55736a20e63dd92c38c4a
imsiddique <imsiddiquek@gmail.com>
2026-02-26 16:34:44 +0400

    authenticate user

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/customer/AuthenticateCustomerApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/customer/AuthenticateCustomerApi.java
@@ -45,21 +45,12 @@ import com.salesmanager.shop.store.security.JWTTokenUtil;
 import com.salesmanager.shop.store.security.PasswordRequest;
 import com.salesmanager.shop.store.security.user.JWTUser;
 import com.salesmanager.shop.utils.AuthorizationUtils;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
-import springfox.documentation.annotations.ApiIgnore;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/v1")
-@Api(tags = {"Customer authentication resource (Customer Authentication Api)"})
-@SwaggerDefinition(tags = {
-    @Tag(name = "Customer authentication resource", description = "Authenticates customer, register customer and reset customer password")
-})
+@Tag(name = "Customer authentication resource", description = "Authenticates customer, register customer and reset customer password")
 public class AuthenticateCustomerApi {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticateCustomerApi.class);
@@ -93,15 +84,11 @@ public class AuthenticateCustomerApi {
      */
     @RequestMapping( value={"/customer/register"}, method=RequestMethod.POST, produces ={ "application/json" })
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(httpMethod = "POST", value = "Registers a customer to the application", notes = "Used as self-served operation",response = AuthenticationResponse.class)
-	@ApiImplicitParams({ @ApiImplicitParam(name = "store", dataType = "string", defaultValue = "DEFAULT"),
-		@ApiImplicitParam(name = "lang", dataType = "string", defaultValue = "en") })
     @ResponseBody
     public ResponseEntity<?> register(
     		@Valid @RequestBody PersistableCustomer customer, 
-			@ApiIgnore MerchantStore merchantStore,
-			@ApiIgnore Language language) throws Exception {
-
+			@Parameter(hidden = true) MerchantStore merchantStore,
+			@Parameter(hidden = true) Language language) throws Exception {
 
             customer.setUserName(customer.getEmailAddress());
             
@@ -155,7 +142,6 @@ public class AuthenticateCustomerApi {
      * @throws AuthenticationException
      */
     @RequestMapping(value = "/customer/login", method = RequestMethod.POST, produces ={ "application/json" })
-    @ApiOperation(httpMethod = "POST", value = "Authenticates a customer to the application", notes = "Customer can authenticate after registration, request is {\"username\":\"admin\",\"password\":\"password\"}",response = ResponseEntity.class)
     @ResponseBody
     public ResponseEntity<?> authenticate(@RequestBody @Valid AuthenticationRequest authenticationRequest) throws AuthenticationException {
 
@@ -213,9 +199,7 @@ public class AuthenticateCustomerApi {
     
 
     @RequestMapping(value = "/auth/customer/password", method = RequestMethod.POST, produces ={ "application/json" })
-    @ApiOperation(httpMethod = "POST", value = "Sends a request to reset password", notes = "Password reset request is {\"username\":\"test@email.com\"}",response = ResponseEntity.class)
     public ResponseEntity<?> changePassword(@RequestBody @Valid PasswordRequest passwordRequest, HttpServletRequest request) {
-
 
         try {
             
--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/user/AuthenticateUserApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/user/AuthenticateUserApi.java
@@ -27,10 +27,7 @@ import com.salesmanager.shop.store.security.AuthenticationRequest;
 import com.salesmanager.shop.store.security.AuthenticationResponse;
 import com.salesmanager.shop.store.security.JWTTokenUtil;
 import com.salesmanager.shop.store.security.user.JWTUser;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * Authenticates a User (Administration purpose)
@@ -39,9 +36,7 @@ import io.swagger.annotations.Tag;
  */
 @Controller
 @RequestMapping("/api/v1")
-@Api(tags = { "User authentication Api" })
-@SwaggerDefinition(tags = {
-		@Tag(name = "User authentication resource", description = "Login for administrator users") })
+@Tag(name = "User authentication resource", description = "Login for administrator users")
 public class AuthenticateUserApi {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticateUserApi.class);
@@ -144,6 +139,5 @@ public class AuthenticateUserApi {
         }
     }
     
-
 
 }
